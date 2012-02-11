50aff9766992e183cbfb9aee0d58f2b06088f2c1
Max Penet <max.penet@gmail.com>
2012-02-11 20:54:54 +0100

    update deps: clojure 1.3  SnakeYAML 1.9

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "YAML encoding and decoding for Clojure using SnakeYAML"
   :url "http://github.com/lancepantz/clj-yaml"
   :dependencies
-    [[org.clojure/clojure "1.2.0"]
-     [org.yaml/snakeyaml "1.5"]])
+    [[org.clojure/clojure "1.3.0"]
+     [org.yaml/snakeyaml "1.9"]])
