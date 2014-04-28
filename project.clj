dfe4f6db341aadb7a20e3835c821a113950eab07
Brian Rowe <browe@rentpath.com>
2014-04-28 18:19:33 -0400

    bump snakeyaml version to 1.11

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-yaml "0.4.0"
+(defproject clj-yaml "0.4.1"
   :description "YAML encoding and decoding for Clojure using SnakeYAML"
   :url "http://github.com/lancepantz/clj-yaml"
   :dependencies
     [[org.clojure/clojure "1.2.0"]
-     [org.yaml/snakeyaml "1.5"]])
+     [org.yaml/snakeyaml "1.11"]])
